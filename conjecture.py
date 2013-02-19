ddc44fc4b20b78716944b66fd1424fcc9ecd8701
Patrick Gaskill <patrick.gaskill@gmail.com>
2013-02-18 20:21:32 -0500

    GraphBrain.expressions is working

--- conjecture.py
+++ conjecture.py
@@ -3,14 +3,14 @@ sys.path.append(".") # Needed to pass Sage's automated testing
 
 from sage.all import *
 from inp import INPGraph
-#import math
+import itertools
 import operator
 
 class GraphBrain(SageObject):
-    _default_graph_invariants = [INPGraph.min_degree]
+    _default_graph_invariants = [Graph.diameter, Graph.radius]
     _default_unary_operators = [sqrt]
     _default_binary_commutative_operators = [operator.add]
-    _default_binary_noncommutative_operators = []
+    _default_binary_noncommutative_operators = [operator.sub]
 
     def __init__(self, name=None, target=None, graphs=[], complexity=1,
                  graph_invariants=_default_graph_invariants,
@@ -26,9 +26,68 @@ class GraphBrain(SageObject):
         self.binary_commutative_operators = binary_commutative_operators
         self.binary_noncommutative_operators = binary_noncommutative_operators
 
-class GraphStatement(SageObject):
-    pass
+    def expressions(self, complexity, _cache=None):
+        r"""
+        Return all possible expressions of the given complexity.
 
+        EXAMPLES:
+
+        ::
+            sage: brain = GraphBrain()
+            sage: brain.graph_invariants = [Graph.diameter, Graph.radius, Graph.order]
+            sage: brain.unary_operators = [sqrt]
+            sage: brain.binary_commutative_operators = [operator.add]
+            sage: brain.binary_noncommutative_operators = [operator.sub]
+            sage: brain.target = Graph.diameter
+            sage: brain.expressions(1)
+            [radius(G), order(G)]
+            sage: brain.target = Graph.order
+            sage: brain.expressions(1)
+            [diameter(G), radius(G)]
+            sage: brain.expressions(2)
+            [sqrt(diameter(G)), sqrt(radius(G))]
+            sage: brain.expressions(3)
+            [diameter(G)^(1/4), radius(G)^(1/4), 0, 2*diameter(G), radius(G) - diameter(G), radius(G) + diameter(G), -radius(G) + diameter(G), radius(G) + diameter(G), 0, 2*radius(G)]
+        """
+        if _cache is None:
+            _cache = {}
+
+        if complexity not in _cache:
+            if complexity < 1:
+                return []
+            elif complexity == 1:
+                _cache[1] = [GraphExpression(self, [inv]) for inv in self.graph_invariants if inv != self.target]
+            else:
+                _cache[complexity] = []
+
+                # Unary operators
+                for expr in self.expressions(complexity - 1, _cache):
+                    for op in self.unary_operators:
+                        copy = expr.copy()
+                        copy.append(op)
+                        _cache[complexity] += [copy]
+
+                # Binary operators
+                for k in range(1, complexity - 1):
+                    for a in self.expressions(k, _cache):
+                        for b in self.expressions(complexity - 1 - k, _cache):
+                            # Noncommutative
+                            for op in self.binary_noncommutative_operators:
+                                copy = a.copy()
+                                copy.extend(b.rpn_stack[:])
+                                copy.append(op)
+                                _cache[complexity] += [copy]
+
+                            # Commutative
+                            if k <= complexity - 1 - k:
+                                for op in self.binary_commutative_operators:
+                                    copy = a.copy()
+                                    copy.extend(b.rpn_stack[:])
+                                    copy.append(op)
+                                    _cache[complexity] += [copy]
+
+        return _cache[complexity]
+        
 class GraphExpression(SageObject):
 
     def __init__(self, brain, rpn_stack):
@@ -37,11 +96,17 @@ class GraphExpression(SageObject):
         self.rpn_stack = rpn_stack
         super(GraphExpression, self).__init__()
 
+    # def __eq__(self, other):
+    #     return self.expression() == other.expression()
+
     def _repr_(self):
         return repr(self.expression())
 
     def _latex_(self):
         return latex(self.expression())
+
+    def copy(self):
+        return GraphExpression(self.brain, self.rpn_stack[:])
 
     def append(self, x):
         r"""
@@ -49,12 +114,12 @@ class GraphExpression(SageObject):
 
         EXAMPLES:
 
-            ::
-                sage: brain = GraphBrain(target=INPGraph.independence_number, graph_invariants=[INPGraph.min_degree, INPGraph.independence_number], unary_operators=[sqrt], binary_commutative_operators=[operator.add], binary_noncommutative_operators=[])
-                sage: expr = GraphExpression(brain, [INPGraph.min_degree])
-                sage: expr.append(sqrt)
-                sage: expr.rpn_stack
-                [<unbound method INPGraph.min_degree>, <function sqrt at ...>]
+        ::
+            sage: brain = GraphBrain(target=INPGraph.independence_number, graph_invariants=[INPGraph.min_degree, INPGraph.independence_number], unary_operators=[sqrt], binary_commutative_operators=[operator.add], binary_noncommutative_operators=[])
+            sage: expr = GraphExpression(brain, [INPGraph.min_degree])
+            sage: expr.append(sqrt)
+            sage: expr.rpn_stack
+            [<unbound method INPGraph.min_degree>, <function sqrt at ...>]
         """
         self.rpn_stack.append(x)
 
@@ -64,12 +129,12 @@ class GraphExpression(SageObject):
 
         EXAMPLES:
 
-            ::
-                sage: brain = GraphBrain(target=INPGraph.independence_number, graph_invariants=[INPGraph.min_degree, INPGraph.independence_number], unary_operators=[sqrt], binary_commutative_operators=[operator.add], binary_noncommutative_operators=[])
-                sage: expr = GraphExpression(brain, [INPGraph.min_degree, sqrt])
-                sage: expr.extend([INPGraph.min_degree, operator.add])
-                sage: expr.rpn_stack
-                [<unbound method INPGraph.min_degree>, <function sqrt at ...>, <unbound method INPGraph.min_degree>, <built-in function add>]
+        ::
+            sage: brain = GraphBrain(target=INPGraph.independence_number, graph_invariants=[INPGraph.min_degree, INPGraph.independence_number], unary_operators=[sqrt], binary_commutative_operators=[operator.add], binary_noncommutative_operators=[])
+            sage: expr = GraphExpression(brain, [INPGraph.min_degree, sqrt])
+            sage: expr.extend([INPGraph.min_degree, operator.add])
+            sage: expr.rpn_stack
+            [<unbound method INPGraph.min_degree>, <function sqrt at ...>, <unbound method INPGraph.min_degree>, <built-in function add>]
         """
         self.rpn_stack.extend(li)
 
